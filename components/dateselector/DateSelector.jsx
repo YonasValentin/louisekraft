d479abfdcb27c1b8a3dddebdf649ce15049ce230
YonasValentin <83760534+YonasValentin@users.noreply.github.com>
2022-05-19 10:22:23 +0200

    [added] mui datetime-picker

--- components/dateselector/DateSelector.jsx
+++ components/dateselector/DateSelector.jsx
@@ -1,69 +1,56 @@
-import { useState } from 'react';
-import CalendarTemplate from 'availability-calendar-react';
+import format from 'date-fns/format';
+import React, { useState } from 'react';
+import daLocale from 'date-fns/locale/da';
+import DateFnsUtils from '@date-io/date-fns';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
-function DateSelector() {
-  const [availability, setAvailability] = useState([]);
-
-  const Calendar = CalendarTemplate({
-    availability,
-    setAvailability,
-    primaryColor: '#CCCCCC',
-    secondaryColor: '#EEEEEE',
-    primaryFontColor: '#444444',
-    fontFamily: 'Roboto',
-    fontSize: 14,
-    startTime: '5:00',
-    endTime: '22:00',
-    availability,
-    setAvailability,
-  });
-  return <Calendar />;
+class LocalizedUtils extends DateFnsUtils {
+  getDatePickerHeaderText(date) {
+    return format(date, 'd MMM yyyy', { locale: this.locale });
+  }
 }
 
-export default DateSelector;
-
-/*
-import DatePicker from 'react-datepicker';
-import setHours from 'date-fns/setHours';
-import setMinutes from 'date-fns/setMinutes';
-import subDays from 'date-fns/subDays';
-import 'react-datepicker/dist/react-datepicker.css';
-import { registerLocale } from 'react-datepicker';
-import da from 'date-fns/locale/da';
-registerLocale('da', da);
-
 function DateSelector() {
-  const [startDate, setStartDate] = useState(
-    setHours(setMinutes(new Date(), 0), 12)
-  );
+  const [value, setValue] = useState(null);
 
-  const isWeekday = (date) => {
-    const day = date.getDay();
-    return day !== 0 && day !== 6;
-  };
+  console.log({ value: value && value.toLocaleString() });
+
+  function disableWeekdays(date) {
+    return date.getDay() === 0 || date.getDay() === 6;
+  }
 
   return (
-    <main>
-      <DatePicker
-        locale='da'
-        showTimeSelect
-        dateFormat='dd/MM/yyyy'
-        timeFormat='HH:mm'
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-        closeOnScroll={true}
-        filterDate={isWeekday}
-        timeIntervals={60}
-        timeCaption='Tid'
-        //inline
-        withPortal
-        minTime={setHours(setMinutes(new Date(), 0), 12)}
-        maxTime={setHours(setMinutes(new Date(), 0), 20)}
+    <LocalizationProvider
+      utils={LocalizedUtils}
+      locale={daLocale}
+      dateAdapter={AdapterDateFns}
+    >
+      <DateTimePicker
+        /*shouldDisableTime={(timeValue, clockType) => {
+          if (clockType === 'hours' && timeValue < 12) {
+            return true;
+          }
+        }}*/
+        //minTime={new Date(0, 1, 2, 3, 4, 5, 6)}
+        //maxTime={new Date(0, 0, 0, 18, 30)}
+        shouldDisableDate={disableWeekdays}
+        minutesStep={30}
+        format='HH:mm dd/MM/yyyy'
+        hideTodayButton
+        disablePast
+        ampm={false}
+        renderInput={(props) => <TextField {...props} />}
+        label='Vælg dato og klokkeslæt'
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
       />
-    </main>
+    </LocalizationProvider>
   );
 }
 
 export default DateSelector;
-
-*/
